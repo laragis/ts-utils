b93d15c0523d1fcee584f4a5709783caa4167821
Truong Thanh Tung <ttungbmt@gmail.com>
2022-11-24 12:29:12 +0700

    Add downloadImage

---
+++ src/downloadImage.ts
@@ -0,0 +1,14 @@
+async function downloadImage(imageSrc: string, imageName: string = 'image') {
+  const image = await fetch(imageSrc)
+  const imageBlog = await image.blob()
+  const imageURL = URL.createObjectURL(imageBlog)
+
+  const link = document.createElement('a')
+  link.href = imageURL
+  link.download = imageName ?? 'image'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
+export default downloadImage
